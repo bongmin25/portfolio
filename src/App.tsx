9cefe07f5afc7b7d193ab7d5b56b5a5f4445184b
bongmin25 <tom.dantas25@gmail.com>
2024-11-19 22:11:56 -0300

    agregando proyecto final a portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import Header from './components/Header';
-import Hero from './components/Hero';
-import About from './components/About';
-import Projects from './components/Projects';
-import Skills from './components/Skills';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -19,8 +19,7 @@ const projects = [
   },
   {
     title: "Appointment App",
-    description:
-      "Plataforma para agendar turnos",
+    description: "Plataforma para agendar turnos",
     technologies: [
       "JavaScript",
       "TypeScript",
@@ -34,20 +33,20 @@ const projects = [
     live: "https://github.com/bongmin25/Appointment-App/",
   },
   {
-    title: "Cinema",
-    description:
-      "Aplicación de Cine",
-    image: "/cinema.png",
+    title: "DevNavigator",
+    description: "Cursos del mundo IT",
+    image: "/devNavigator.png",
     technologies: [
       "JavaScript",
       "React.js",
       "HTML",
+      "Tailwind",
       "Express",
       "Next.js",
       "PostgreSQL",
     ],
-    github: "https://github.com/bongmin25/Cinema",
-    live: "https://github.com/bongmin25/Cinema",
+    github: "https://github.com/DevNavigator/DevNavigatorProject",
+    live: "https://dev-navigator.vercel.app/",
   },
 ];
 
@@ -55,21 +54,31 @@ const Projects: React.FC = () => {
   return (
     <section id="projects" className="py-20 bg-gray-900">
       <div className="container mx-auto px-4">
-        <h2 className="text-4xl font-bold mb-8 text-center">Mis Proyectos</h2>
+        <h2 className="text-4xl font-bold mb-8 text-center text-white">
+          Mis Proyectos
+        </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {projects.map((project, index) => (
             <div
               key={index}
               className="bg-gray-800 rounded-lg overflow-hidden shadow-lg"
             >
-              <img
-                src={project.image}
-                alt={project.title}
-                className="w-full h-48 object-cover hover:transform hover:scale-125 transition"
-              />
+              <div className="relative overflow-hidden group">
+                <img
+                  src={project.image}
+                  alt={project.title}
+                  className="w-full h-48 object-cover transition-transform duration-300 ease-in-out group-hover:scale-110"
+                />
+                <div className="absolute inset-0 bg-black bg-opacity-60 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                  <p className="text-white text-center px-4">
+                    {project.description}
+                  </p>
+                </div>
+              </div>
               <div className="p-6">
-                <h3 className="text-xl font-semibold mb-2">{project.title}</h3>
-                <p className="text-gray-300 mb-4">{project.description}</p>{" "}
+                <h3 className="text-xl font-semibold mb-2 text-white">
+                  {project.title}
+                </h3>
                 <div className="flex flex-wrap gap-2 mt-2">
                   {project.technologies.map((tech, i) => (
                     <span
@@ -85,7 +94,7 @@ const Projects: React.FC = () => {
                     href={project.github}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="flex items-center text-orange-500 hover:text-orange-800 transition"
+                    className="flex items-center text-orange-500 hover:text-orange-400 transition"
                   >
                     <Github size={20} className="mr-1" /> GitHub
                   </a>
@@ -93,7 +102,7 @@ const Projects: React.FC = () => {
                     href={project.live}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="flex items-center text-rose-900 hover:text-rose-600 transition"
+                    className="flex items-center text-rose-500 hover:text-rose-400 transition"
                   >
                     <ExternalLink size={20} className="mr-1" /> Ver proyecto
                   </a>
